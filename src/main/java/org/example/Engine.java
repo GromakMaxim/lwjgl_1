62bd55496b8ae5f2bc31a25b739620c042496ffd
MaximGromak <maximgromak@gmail.com>
2022-12-18 18:08:35 +1000

    draw shapes

--- src/main/java/org/example/Engine.java
+++ src/main/java/org/example/Engine.java
@@ -3,7 +3,13 @@ package org.example;
 import lombok.Getter;
 import org.example.controller.KeyboardController;
 import org.example.controller.MouseController;
+import org.example.shapes.Shapes;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.FloatBuffer;
 
 
 public class Engine {
@@ -20,6 +26,14 @@ public class Engine {
         this.mouseController = new MouseController(this.engineWindow.getWindowId());
     }
 
+    public FloatBuffer storeDataInFloatBuffer(float[] data) {
+        FloatBuffer buffer = MemoryUtil.memAllocFloat(data.length);
+        buffer.put(data);
+        buffer.flip();
+
+        return buffer;
+    }
+
     public void run() {
         this.update();
     }
@@ -28,6 +42,22 @@ public class Engine {
      * обновление каждый фрейм
      */
     public void update() {
+
+        int vaoId = GL30.glGenVertexArrays();
+        GL30.glBindVertexArray(vaoId);
+
+        int vboId = GL30.glGenBuffers();
+        GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, vboId);
+        FloatBuffer buffer = this.storeDataInFloatBuffer(Shapes.SQUARE.getArr());
+        GL30.glBufferData(GL30.GL_ARRAY_BUFFER, buffer, GL30.GL_STATIC_DRAW);
+        GL30.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+        MemoryUtil.memFree(buffer);
+        GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, vboId);
+
+
+        GL30.glBindVertexArray(vaoId);
+
+
         while (!this.engineWindow.isRequestClosed()) {
 
             if (this.keyboardController.keyPressed(GLFW.GLFW_KEY_UP)) System.out.println("up");
@@ -44,6 +74,16 @@ public class Engine {
             // обработка нажатий
             keyboardController.handleInput();
             mouseController.handleInput();
+
+            GL11.glClearColor(0, 1, 0, 1);
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+
+            GL30.glBindVertexArray(vaoId);
+            GL30.glEnableVertexAttribArray(0);
+            GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, Shapes.SQUARE.getLength() / 3);
+            GL30.glDisableVertexAttribArray(0);
+            GL30.glBindVertexArray(vaoId);
+
             // рендеринг
             this.engineWindow.update();
         }
---
+++ src/main/java/org/example/shapes/Shapes.java
@@ -0,0 +1,34 @@
+package org.example.shapes;
+
+import lombok.Getter;
+
+public enum Shapes {
+    TRIANGLE(new float[]{0.0f, 0.5f, 0f,
+            -0.5f, -0.5f, 0f,
+            0.5f, -0.5f, 0f}),
+
+    SQUARE(new float[]{0.5f, 0.5f, 0,
+            -0.5f, 0.5f, 0,
+            -0.5f, -0.5f, 0,
+            0.5f, 0.5f, 0,
+            -0.5f, -0.5f, 0,
+            0.5f, -0.5f, 0}),
+
+    ROMB(new float[]{0.0f, 0.5f, 0f,
+            -0.5f, 0.0f, 0f,
+            0.5f, 0.0f, 0f,
+            0.5f, 0.0f, 0f,
+            -0.5f, 0.0f, 0f,
+            0.0f, -0.5f, 0f});
+
+    @Getter
+    private float[] arr;
+
+    public int getLength(){
+        return this.arr.length;
+    }
+
+    Shapes(float[] arr) {
+        this.arr = arr;
+    }
+}
