f3e5e4b84ee07310b42f5f6332671bcda9b09e94
MaximGromak <maximgromak@gmail.com>
2022-12-19 22:42:38 +1000

    GL46C

--- src/main/java/org/example/Engine.java
+++ src/main/java/org/example/Engine.java
@@ -13,6 +13,12 @@ import org.lwjgl.system.MemoryUtil;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 
+import static org.lwjgl.opengl.GL11C.GL_UNSIGNED_INT;
+import static org.lwjgl.opengl.GL15C.*;
+import static org.lwjgl.opengl.GL20C.*;
+import static org.lwjgl.opengl.GL30C.glBindVertexArray;
+import static org.lwjgl.opengl.GL30C.glGenVertexArrays;
+
 
 public class Engine {
     @Getter
@@ -53,29 +59,29 @@ public class Engine {
      */
     public void update() {
 
-        int vaoId = GL30.glGenVertexArrays();
+        int vaoId = glGenVertexArrays();
         GL30.glBindVertexArray(vaoId);
 
 
-        int iboId= GL30.glGenBuffers();
-        GL30.glBindBuffer(GL30.GL_ELEMENT_ARRAY_BUFFER, iboId);
+        int iboId = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, iboId);
         IntBuffer intBuffer = this.storeDataInIntBuffer(ShapesInt.SQUARE.getArray());
-        GL30.glBufferData(GL30.GL_ELEMENT_ARRAY_BUFFER, intBuffer, GL30.GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, intBuffer, GL_STATIC_DRAW);
         MemoryUtil.memFree(intBuffer);
 
         //генерируем Vertex Buffer и связываем его
-        int vboId = GL30.glGenBuffers();
-        GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, vboId);
+        int vboId = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboId);
         FloatBuffer floatBuffer = this.storeDataInFloatBuffer(Shapes.SQUARE.getArr());
-        GL30.glBufferData(GL30.GL_ARRAY_BUFFER, floatBuffer, GL30.GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, floatBuffer, GL_STATIC_DRAW);
         MemoryUtil.memFree(floatBuffer);
         //создаем атрибут дл¤ вершины, указываем id, тип данных.
-        GL30.glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
+        glVertexAttribPointer(0, 3, GL11.GL_FLOAT, false, 0, 0);
 
 
         //разв¤зываем Vbo's и сам лист Vao
-        GL30.glBindBuffer(GL30.GL_ARRAY_BUFFER, vboId);
-        GL30.glBindVertexArray(vaoId);
+        glBindBuffer(GL_ARRAY_BUFFER, vboId);
+        glBindVertexArray(vaoId);
 
 
         while (!this.engineWindow.isRequestClosed()) {
@@ -95,15 +101,15 @@ public class Engine {
             keyboardController.handleInput();
             mouseController.handleInput();
 
-            GL11.glClearColor(0, 1, 0, 1);
-            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+            glClearColor(0, 1, 0, 1);
+            glClear(GL11.GL_COLOR_BUFFER_BIT);
 
-            GL30.glBindVertexArray(vaoId);
-            GL30.glEnableVertexAttribArray(0);
+            glBindVertexArray(vaoId);
+            glEnableVertexAttribArray(0);
             //GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, Shapes.SQUARE.getLength() / 3);
-            GL11.glDrawElements(GL11.GL_TRIANGLES, ShapesInt.SQUARE.getLength(), GL11.GL_UNSIGNED_INT, 0);
-            GL30.glDisableVertexAttribArray(0);
-            GL30.glBindVertexArray(vaoId);
+            glDrawElements(GL11.GL_TRIANGLES, ShapesInt.SQUARE.getLength(), GL_UNSIGNED_INT, 0);
+            glDisableVertexAttribArray(0);
+            glBindVertexArray(vaoId);
 
             // рендеринг
             this.engineWindow.update();
